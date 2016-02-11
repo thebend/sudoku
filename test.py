87a6b7ee847dd04c9a5f57bfd35a4796841198ce
thebend <ben.davidson@gmail.com>
2016-02-10 18:10:57 -0800

    Consistent test case formatting

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
 import sudoku
 data = sudoku.get_puzzle('puzzles/1.txt')
 print sudoku.puzzle_string(data)
-'''
-while sudoku.reduce_pass(data):
-    print
-    print
-    print sudoku.puzzle_string(data)
-'''
 sudoku.solve(data)
 print sudoku.puzzle_string(data)
 
